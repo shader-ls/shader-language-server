d0cd9bfa15b175be30e5312215777c82a3e055e9
JenChieh <jcs090218@gmail.com>
2023-08-23 17:51:10 -0700

    fix: Make sure word has no newline

--- Server/Buffer.cs
+++ Server/Buffer.cs
@@ -66,7 +66,7 @@ namespace ShaderLS
                 on = word;
             }
 
-            return on;
+            return on.Trim();
         }
 
         private void SetWordsInDocuments(string text)
